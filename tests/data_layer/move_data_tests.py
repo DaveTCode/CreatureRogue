4de658eee1c85127316202a53c595f6adbf08314
David Tyler <davetcode@gmail.com>
2016-01-02 19:42:18 +0000

    Added tests for the move_data class

--- tests/data_layer/move_data_tests.py
+++ tests/data_layer/move_data_tests.py
@@ -2,8 +2,9 @@ import unittest
 
 from CreatureRogue.data_layer.ailment import Ailment
 from CreatureRogue.data_layer.move_data import MoveData
-from CreatureRogue.data_layer.type import Type
 from CreatureRogue.data_layer.move_target import MoveTarget
+from CreatureRogue.data_layer.stat import Stat
+from CreatureRogue.data_layer.type import Type
 
 
 class MoveDataTests(unittest.TestCase):
@@ -13,3 +14,47 @@ class MoveDataTests(unittest.TestCase):
                         min_hits=0, max_hits=0, stat_changes={}, attack_stat=None, defence_stat=None,
                         accuracy_stat=None, evasion_stat=None, target=MoveTarget("None", "None", "None"),
                         ailment=Ailment(ailment_id=0, name="None"))
+
+    def test_is_attack_move_true(self):
+        m = self.create_default("")
+        m.attack_stat = Stat(name="stat", short_name="st")
+        self.assertTrue(m.damage_move())
+
+    def test_is_attack_move_false(self):
+        m = self.create_default("")
+        m.attack_stat = None
+        self.assertFalse(m.damage_move())
+
+    def test_is_stat_change_move_true_single_stat(self):
+        m = self.create_default("")
+        m.stat_changes[Stat(name="stat", short_name="st")] = 1
+        self.assertTrue(m.stat_change_move())
+
+    def test_is_stat_change_move_true_multiple_stats(self):
+        m = self.create_default("")
+        m.stat_changes[Stat(name="stat", short_name="st")] = 0
+        m.stat_changes[Stat(name="stat2", short_name="st2")] = 1
+        self.assertTrue(m.stat_change_move())
+
+    def test_is_stat_change_move_false_no_stats(self):
+        m = self.create_default("")
+        self.assertFalse(m.stat_change_move())
+
+    def test_is_stat_change_move_false_one_stat(self):
+        m = self.create_default("")
+        m.stat_changes[Stat(name="stat", short_name="st")] = 0
+        self.assertFalse(m.stat_change_move())
+
+    def test_is_stat_change_move_false_multiple_moves(self):
+        m = self.create_default("")
+        m.stat_changes[Stat(name="stat", short_name="st")] = 0
+        m.stat_changes[Stat(name="stat2", short_name="st2")] = 0
+        self.assertFalse(m.stat_change_move())
+
+    def test_str(self):
+        m = self.create_default("")
+        self.assertEqual("", str(m))
+
+    def test_str_real_name(self):
+        m = self.create_default("Pound")
+        self.assertEqual("Pound", str(m))
