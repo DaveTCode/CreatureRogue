a15bb063e3b5bd4b227a81ff8bfd2e502e24bfec
David Tyler <davetcode@gmail.com>
2016-01-03 19:09:52 +0000

    Added typing to location_area object

--- CreatureRogue/data_layer/location_area.py
+++ CreatureRogue/data_layer/location_area.py
@@ -1,21 +1,34 @@
 import functools
 import random
+from typing import Optional, Sequence
+
+from CreatureRogue.data_layer.encounter import Encounter
+from CreatureRogue.data_layer.location import Location
 
 
 class LocationArea:
-    def __init__(self, identifier, name, location, walk_encounters, walk_encounter_rate):
+    """
+    A location area is the region of the map which contains a certain set of
+    encounters.
+    """
+    def __init__(self, identifier: str, name: str, location: Location, walk_encounters: Sequence[Encounter]):
         self.location = location
         self.identifier = identifier
         self.name = name
-        self.walk_encounters = walk_encounters
-        self.walk_encounter_rate = walk_encounter_rate
+        self.walk_encounters = walk_encounters  # TODO - Convert walk_encounters to an encounters map that takes an encounter type (id?) and maps to a set of encounters instead
 
-    def get_encounter(self):
+    def get_encounter(self) -> Optional[Encounter]:
         """
             Select between all of the available encounters in the location area.
 
             This is a weighted random selection and can return None if there were
             no encounters available.
+
+            Note that this is not intended to determine whether an encounter
+            should occur, this will always return an encounter if one exists
+            for the area.
+
+            TODO - This currently assumes that the encounter type is walking.
         """
         total_rarity = functools.reduce(lambda x, y: x + y.rarity, self.walk_encounters, 0)
         rand = random.randint(0, total_rarity if total_rarity == 0 else total_rarity - 1)
