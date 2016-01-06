c5ea5f2a13e321f441793c6d14ad276fccbe32cb
David Tyler <davetcode@gmail.com>
2016-01-06 20:24:44 +0000

    Rewrote the map test function to use the new style of maps

--- map_test.py
+++ map_test.py
@@ -4,7 +4,8 @@ import CreatureRogue.creature_creator as creature_creator
 import CreatureRogue.settings as settings
 from CreatureRogue.game import Game
 from CreatureRogue.models.creature import Creature
-from CreatureRogue.models.map import Map
+from CreatureRogue.data_layer.map_loader import MapData, MapLoader
+from CreatureRogue.data_layer.region import Region
 from CreatureRogue.models.move import Move
 from CreatureRogue.models.player import Player
 from CreatureRogue.states.map_state import MapState
@@ -13,6 +14,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("x", type=int)
     parser.add_argument("y", type=int)
+    # TODO - Allow for selection of other regions (when they're stored in the database
     args = parser.parse_args()
 
     game = Game(settings.SCREEN_WIDTH, settings.SCREEN_HEIGHT, settings.TITLE, settings.FONT)
@@ -20,7 +22,11 @@ if __name__ == "__main__":
     game.init()
 
     # TODO - Fix this now that the map data isn't loaded automatically
-    game.game_data.player = Player("Test Player", game.static_game_data, Map(kanto.name, kanto.tiles), args.x, args.y)
+    kanto_region = Region(id=1, identifier="kanto", name="kanto")
+    map_data = MapLoader(db_file=settings.DB_FILE).load_map(region=kanto_region,
+                                                            tile_types=game.static_game_data.map_data_tile_types,
+                                                            default_tile_type=game.static_game_data.map_data_tile_types[11])
+    game.game_data.player = Player("Test Player", game.static_game_data, map_data, args.x, args.y)
     game.state = MapState(game, game.game_data, game.map_renderer)
 
     attacking_id = int(input("Enter the pokedex number of the player creature: "))
@@ -31,6 +37,6 @@ if __name__ == "__main__":
     attacking_moves = [Move(move_data) for move_data in attacking_species.move_data_at_level(attacking_level)]
 
     game.game_data.player.creatures.append(Creature(attacking_species, attacking_level, None, None, creature_creator.random_stat_values(game.static_game_data.stats, 1, 15), creature_creator.zero_stat_values(game.static_game_data.stats), False, attacking_moves, 1))
-    game.game_data.player.pokeballs[game.game_data.player.pokeballs.keys()[0]] = 100
+    #game.game_data.player.pokeballs[game.game_data.player.pokeballs.keys()[0]] = 100
 
     game.game_loop()
