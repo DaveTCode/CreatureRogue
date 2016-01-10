5a4386cff5c2dfe82f7987bf1448c855d9c5b8bc
David Tyler <davetcode@gmail.com>
2016-01-10 16:09:21 +0000

    Added typing to the TypeChart class - precursor to adding UTs

--- CreatureRogue/data_layer/type_chart.py
+++ CreatureRogue/data_layer/type_chart.py
@@ -1,17 +1,28 @@
+from typing import Mapping
+
+from CreatureRogue.data_layer.type import Type
+
+
 class TypeChart:
-    def __init__(self, chart):
+    def __init__(self, chart: Mapping[Type, Mapping[Type, int]]):
         self.chart = chart
 
-    def damage_modifier(self, attacking_type, defending_type):
+    def damage_modifier(self, attacking_type: Type, defending_type: Type) -> int:
         """
             The type chart hold information on how attacks of all types affect
             creatures of all types. This function allows us to query that
             information.
+
+            :param defending_type: A species/move type.
+            :param attacking_type: A species/move type.
+
+            :return: The integer factor which corresponds to the percentage
+            damage adjustment (so 100 is default).
         """
         if attacking_type in self.chart and defending_type in self.chart[attacking_type]:
             return self.chart[attacking_type][defending_type]
         else:
-            return 1
+            return 100
 
     def __str__(self):
         type_chart_str = ""
