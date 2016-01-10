6978a50f4cfe6e2fc9ec40a4f540f8d0eb6bffd6
David Tyler <davetcode@gmail.com>
2016-01-10 17:27:15 +0000

    Added tests to 100% cover the location area rect data layer class and added typing.

--- CreatureRogue/data_layer/location_area_rect.py
+++ CreatureRogue/data_layer/location_area_rect.py
@@ -1,5 +1,5 @@
 class LocationAreaRect:
-    def __init__(self, location_area_id, x1, y1, x2, y2):
+    def __init__(self, location_area_id: int, x1: int, y1: int, x2: int, y2: int):
         self.location_area_id = location_area_id
         self.x1 = x1
         self.y1 = y1
